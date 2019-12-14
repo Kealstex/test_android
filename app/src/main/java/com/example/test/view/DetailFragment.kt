b10c0ee8f093490f19f3279cdacb619baa6e2e61
Kealstex <Kealstex@yandex.com>
2019-12-14 18:19:10 +0300

    add navigation vs fragment TODO::: detailFragment: txtView = null

--- app/src/main/java/com/example/test/view/DetailFragment.kt
+++ app/src/main/java/com/example/test/view/DetailFragment.kt
@@ -10,11 +10,13 @@ import androidx.navigation.Navigation
 
 import com.example.test.R
 import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class DetailFragment : Fragment() {
+    private var moneyuuid = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +30,13 @@ class DetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         btnToList.setOnClickListener{
             val action = DetailFragmentDirections.actionDetailFragmentToListFragment()
+
+            arguments?.let {
+                moneyuuid = DetailFragmentArgs.fromBundle(it).moneyUuid
+                txtDetail.text = moneyuuid.toString()
+            }
+
             Navigation.findNavController(it).navigate(action)
         }
     }
-
-
 }
